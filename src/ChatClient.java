36565bc55b2cb280d723f9f3958bdd4c96c47743
tasnidmahin <tasnidmahin@gmail.com>
2018-09-21 23:19:26 +0600

    chat client updated 2

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,156 +1,126 @@
-
-import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.io.*;
-import java.net.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
-public class ChatClient extends JFrame implements Runnable{
-    
-    Socket socket;
-    JTextArea ta;
-    JButton send,logout;
-    JTextField tf;
-    
-    
-    Thread thread;
-    
-    DataInputStream din;
-    DataOutputStream dout;
-    
-    String LoginName;
+/**
+ * A simple Swing-based client for the chat server.  Graphically
+ * it is a frame with a text field for entering messages and a
+ * textarea to see the whole dialog.
+ *
+ * The client follows the Chat Protocol which is as follows.
+ * When the server sends "SUBMITNAME" the client replies with the
+ * desired screen name.  The server will keep sending "SUBMITNAME"
+ * requests as long as the client submits screen names that are
+ * already in use.  When the server sends a line beginning
+ * with "NAMEACCEPTED" the client is now allowed to start
+ * sending the server arbitrary strings to be broadcast to all
+ * chatters connected to the server.  When the server sends a
+ * line beginning with "MESSAGE " then all characters following
+ * this string should be displayed in its message area.
+ */
+public class ChatClient {
 
-    public ChatClient(String login) throws UnknownHostException,IOException {
-        super(login);
-        LoginName = login;
-        
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e){
-                try {
-                    dout.writeUTF(LoginName + " " + "LOGOUT");
-                    System.exit(1);
-                } catch (IOException ex) {
-                    //Logger.getLogger(ChatClient.class.getName()).log(Level.SEVERE, null, ex);
-                    ex.printStackTrace();
-                }
-            }
-        });
-        
-        ta = new JTextArea(18,50);
-        tf = new JTextField(50);
-        
-        tf.addKeyListener(new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {
-                //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-            }
+    BufferedReader in;
+    PrintWriter out;
+    JFrame frame = new JFrame("Chatter");
+    JTextField textField = new JTextField(40);
+    JTextArea messageArea = new JTextArea(20, 80);
 
-            @Override
-            public void keyPressed(KeyEvent e) {
-                //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                if(e.getKeyCode() == KeyEvent.VK_ENTER){
-                    try {
-                        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                        if(tf.getText().length()>0)    
-                            dout.writeUTF(LoginName + " " + "DATA " + tf.getText().toString());
-                        tf.setText("");
-                    } catch (IOException ex) {
-                        //Logger.getLogger(ChatClient.class.getName()).log(Level.SEVERE, null, ex);
-                        ex.printStackTrace();
-                    }
-                }
-            }
+    /**
+     * Constructs the client by laying out the GUI and registering a
+     * listener with the textfield so that pressing Return in the
+     * listener sends the textfield contents to the server.  Note
+     * however that the textfield is initially NOT editable, and
+     * only becomes editable AFTER the client receives the NAMEACCEPTED
+     * message from the server.
+     */
+    public ChatClient() {
 
-            @Override
-            public void keyReleased(KeyEvent e) {
-                //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-            }
-        });
-        
-        send = new JButton("Send");
-        logout = new JButton("Logout");
-        
-        send.addActionListener(new ActionListener() {
-            @Override
+        // Layout GUI
+        textField.setEditable(true);
+        messageArea.setEditable(false);
+        frame.getContentPane().add(textField, "North");
+        frame.getContentPane().add(new JScrollPane(messageArea), "Center");
+        frame.pack();
+
+        // Add Listeners
+        textField.addActionListener(new ActionListener() {
+            /**
+             * Responds to pressing the enter key in the textfield by sending
+             * the contents of the text field to the server.    Then clear
+             * the text area in preparation for the next message.
+             */
             public void actionPerformed(ActionEvent e) {
-                try {
-                    //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                    if(tf.getText().length()>0)
-                        dout.writeUTF(LoginName + " " + "DATA " + tf.getText().toString());
-                    tf.setText("");
-                } catch (IOException ex) {
-                    //Logger.getLogger(ChatClient.class.getName()).log(Level.SEVERE, null, ex);
-                    ex.printStackTrace();
-                }
+                out.println(textField.getText());
+                textField.setText("");
             }
         });
-        
-        logout.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                    dout.writeUTF(LoginName + " " + "LOGOUT" );
-                    System.exit(1);
-                } catch (IOException ex) {
-                    //Logger.getLogger(ChatClient.class.getName()).log(Level.SEVERE, null, ex);
-                    ex.printStackTrace();
-                }
-            }
-        });
-        
-        socket = new Socket("localhost",5217);
-        
-        din = new DataInputStream(socket.getInputStream());
-        dout = new DataOutputStream(socket.getOutputStream());
-        
-        dout.writeUTF(LoginName);
-        dout.writeUTF(LoginName + " " + "LOGIN");
-        
-        thread = new Thread(this);
-        thread.start();
-        setup();
-    }
-    
-    private void setup() {
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        
-        setSize(600,400);
-        
-        JPanel panel = new JPanel();
-        
-        panel.add(new JScrollPane(ta));
-        panel.add(tf);
-        panel.add(send);
-        panel.add(logout);
-        
-        add(panel);
-        
-        setVisible(true);
     }
 
-    @Override
-    public void run() {
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        while(true){            
-            try {
-                ta.append("\n" + din.readUTF());
-            } catch (IOException ex) {
-                //Logger.getLogger(ChatClient.class.getName()).log(Level.SEVERE, null, ex);
-                ex.printStackTrace();
+    /**
+     * Prompt for and return the address of the server.
+     */
+    private String getServerAddress() {
+        return JOptionPane.showInputDialog(
+            frame,
+            "Enter IP Address of the Server:",
+            "Welcome to the Chatter",
+            JOptionPane.QUESTION_MESSAGE);
+    }
+
+    /**
+     * Prompt for and return the desired screen name.
+     */
+    private String getName() {
+        return JOptionPane.showInputDialog(
+            frame,
+            "Choose a screen name:",
+            "Screen name selection",
+            JOptionPane.PLAIN_MESSAGE);
+    }
+
+    /**
+     * Connects to the server then enters the processing loop.
+     */
+    private void run() throws IOException {
+
+        // Make connection and initialize streams
+        String serverAddress = getServerAddress();
+        Socket socket = new Socket(serverAddress, 9001);
+        in = new BufferedReader(new InputStreamReader(
+            socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+
+        // Process all messages from server, according to the protocol.
+        while (true) {
+            String line = in.readLine();
+            if (line.startsWith("SUBMITNAME")) {
+                out.println(getName());
+            } else if (line.startsWith("NAMEACCEPTED")) {
+                textField.setEditable(true);
+            } else if (line.startsWith("MESSAGE")) {
+                messageArea.append(line.substring(8) + "\n");
             }
         }
     }
-    /*public static void main(String[] args) throws UnknownHostException,IOException {
-        ChatClient client = new ChatClient("User1");
-    }*/
-    
+
+    /**
+     * Runs the client as an application with a closeable frame.
+     */
+    public static void main(String[] args) throws Exception {
+        ChatClient client = new ChatClient();
+        client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        client.frame.setVisible(true);
+        client.run();
+    }
 }
